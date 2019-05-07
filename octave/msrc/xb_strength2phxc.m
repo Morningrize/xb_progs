a012f3312917bc6d5fd79909d94ac62140bb7bab
Lorenzo Zanetti <lzanetti@ikp.tu-darmstadt.de>
2019-05-07 16:17:59 +0200

    Added a tool to convert oscillator strengths into photoabsorbtion cross section and a funny mathematical object. This latter one works. The strength thingie: not yet. There is some unit of measure confusion: B(pl) is in units of whatever the fuck, I'll fix the dimensions tomorrow --It'll have to get an area out if the formula.

---
+++ octave/msrc/xb_strength2phxc.m
@@ -0,0 +1,37 @@
+%this function calculates a photoabsorption cross section from an oscillator strength
+%"strength" as defined B( El, J_i->J_f ) = 1/(2Ji-1)*|<J_f||M(El)||J_i>|^2
+%
+% phxs = xb_strength2phxb( energy, fsd_nrg, strength, l );
+%
+%arguments:
+% energy : either an array or a value, the excitation energy to target. KeV.
+% fsd_nrg : the final state density as a function of the excitation energy.
+%           Either an array with the same dimensions of energy or a function handle.
+% strength : the strength in question.
+% l : the multipolarity order.
+%NOTE: the kind (magnetic, electric) of the strength --> the returned cross section
+%      is not an explicit parameter: it comes with the number of the strength.
+%returns:
+% phxs: the photoabsorption cross section corresponding to the given energy(es)
+%       and strength
+%NOTE: this function does not sum over final states, but considers one single B(pl, i->f )
+%      at one time. If you need to sum, eval more time and then sum for phxs^pl.
+
+function phxs = xb_strength2phxc( nrg, fsd, strength, l )
+    if is_function_handle( fsd )
+        fsd = fsd( nrg );
+    elseif size( fsd ) ~= size( nrg )
+        error( 'fsd_nrg and energy MUST have the same dimension.' );
+    end
+    
+    _hbar = 6.5821195144e-16; %reduced Planck constant, eV*s
+    _c = 299792458; %speed of light, m/s
+    
+    nrg = nrg*1e3; %KeV to eV
+    
+    ki = nrg/(_hbar*_c);
+    phxs = (2*pi)^3*(l+1)/(l*xb_semifact( 2*l+1 )^2) * ...
+           fsd.*ki.^(2*l-1)*strength;
+end
+    
+    
