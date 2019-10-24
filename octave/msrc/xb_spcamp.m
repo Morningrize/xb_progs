6f5d3220d4d1e4de622b13e6b743b45ef202cdf6
Lorenzo Zanetti <lzanetti@ikp.tu-darmstadt.de>
2019-10-24 15:17:34 +0200

    Added an utility to amplify the spectrum a bit more nouanced than just multiplying the binZ.

---
+++ octave/msrc/xb_spcamp.m
@@ -0,0 +1,38 @@
+%this utility takes in a spectrum and "amplifies it" --i.E. it will treat it as a probability distribution
+%and produce a random number generator with it, and then produce a spectrum with more (or less) counts in
+%the integral, as requested. Smoothing of the original spectrum may happen.
+%
+% [aspc, aspcerr] = xb_spcamp( spc, bin, amp_factor, [smooth] )
+%
+%parameters:
+% -- spc: the original spectrum
+% -- bin: the binning of the spectrum
+% -- amp_factor: the factor with which to multiply the original integral ( < 1 is allowed )
+% -- smooth: optionally, an integer, odd factor for a Savitzky-Golay filetrin of the original spectrum
+%returns:
+% -- aspc: the amplified spectrum
+% -- spcerr: the error on the amplified spectrum (relative error is conserved)
+
+function [aspc, aspcerr] = xb_spcamp( spc, bin, amp_factor, smooth )
+    intg = sum( spc );
+    aintg = round( amp_factor*intg ); %has to be an integer, of course
+    if nargin == 4
+        try
+            pkg load signal
+        catch
+            error( 'I need the package "signal" to do smoothing' );
+        end
+        smooth = round( smooth );
+        if mod( smooth, 2 ) == 0; smooth += 1; end
+        spc = sgolayfilt( spc, smooth );
+        intg = sum( spc );
+    end
+    erel = sqrt( spc )./spc;
+
+    spc /= intg;
+    gen = xb_arbitrary_distro( spc, bin );
+
+    deposits = gen( 1, aintg );
+    aspc = hist( deposits, bin );
+    aspcerr = aspc.*erel;
+end
